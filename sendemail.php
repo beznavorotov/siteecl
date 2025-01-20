db4e32f73a90a1fd36fbefc9b5a0036681171158
beznavorotov <beznavorotov@gmail.com>
2025-01-20 13:51:40 +0200

    upgrade contact file

--- sendemail.php
+++ sendemail.php
@@ -2,7 +2,7 @@
 
 // Define some constants
 define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+define( "RECIPIENT_EMAIL", "admin@eclogistic.com.ua" );
 
 
 // Read the form values
